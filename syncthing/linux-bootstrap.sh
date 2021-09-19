150655fc496ddb809a2b0d392c2fa9af16db943d
Patroclus <patches72790@gmail.com>
2021-09-19 15:10:23 -0500

    update stignore

--- syncthing/linux-bootstrap.sh
+++ syncthing/linux-bootstrap.sh
@@ -14,3 +14,29 @@ printf "Package: *\nPin: origin apt.syncthing.net\nPin-Priority: 990\n" | sudo t
 
 sudo apt-get update
 sudo apt-get install syncthing
+
+
+# TODO -- need to also setup systemctl service for installing
+
+#[Unit]
+#Description=Syncthing - Open Source Continuous File Synchronization
+#Documentation=man:syncthing(1)
+#After=network.target
+#
+#[Service]
+#User=pi
+#ExecStart=/usr/bin/syncthing -no-browser -no-restart -logflags=0
+#Restart=on-failure
+#RestartSec=5
+#SuccessExitStatus=3 4
+#RestartForceExitStatus=3 4
+#
+## Hardening
+#ProtectSystem=full
+#PrivateTmp=true
+#SystemCallArchitectures=native
+#MemoryDenyWriteExecute=true
+#NoNewPrivileges=true
+#
+#[Install]
+#WantedBy=multi-user.target
