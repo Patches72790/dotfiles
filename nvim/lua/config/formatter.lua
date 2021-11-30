5d4a73458fa7fd8296eec3e4e212cd84ff890028
Patrick Harvey <patches72790@gmail.com>
2021-11-30 10:19:18 -0600

    added formatter configs

--- nvim/lua/config/formatter.lua
+++ nvim/lua/config/formatter.lua
@@ -19,6 +19,66 @@ formatter.setup {
                             stdin = true 
                         }
             end
-        }
+        },
+        html = {
+            function()
+                return  {
+                            exe = 'prettier', 
+                            args = {"--stdin-filepath", 
+                                    vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
+                                    '--config ' .. os.getenv("HOME") .. "/Projects/project_configs/.prettierrc.js"
+                                   },
+                            stdin = true 
+                        }
+            end
+        },
+        typescriptreact = {
+            function()
+                return  {
+                            exe = 'prettier',
+                            args = {"--stdin-filepath",
+                                    vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
+                                    '--config ' .. os.getenv("HOME") .. "/Projects/project_configs/.prettierrc.js"
+                                   },
+                            stdin = true
+                        }
+            end
+        },
+        typescript = {
+            function()
+                return  {
+                            exe = 'prettier',
+                            args = {"--stdin-filepath",
+                                    vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
+                                    '--config ' .. os.getenv("HOME") .. "/Projects/project_configs/.prettierrc.js"
+                                   },
+                            stdin = true
+                        }
+            end
+        },
+        javascript = {
+            function()
+                return  {
+                            exe = 'prettier',
+                            args = {"--stdin-filepath",
+                                    vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
+                                    '--config ' .. os.getenv("HOME") .. "/Projects/project_configs/.prettierrc.js"
+                                   },
+                            stdin = true
+                        }
+            end
+        },
+        javascriptreact = {
+            function()
+                return  {
+                            exe = 'prettier',
+                            args = {"--stdin-filepath",
+                                    vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
+                                    '--config ' .. os.getenv("HOME") .. "/Projects/project_configs/.prettierrc.js"
+                                   },
+                            stdin = true
+                        }
+            end
+        },
     }
 }
