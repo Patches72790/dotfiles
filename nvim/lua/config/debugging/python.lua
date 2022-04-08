8dce51478989ff4b173ac594710d705be10152cf
Patrick <patches72790@gmail.com>
2022-04-08 17:24:02 -0500

    added gunicorn test config for nvim dap

--- nvim/lua/config/debugging/python.lua
+++ nvim/lua/config/debugging/python.lua
@@ -1,17 +1,18 @@
 local M = {}
 
 local custom_configurations = {
-	{
-		type = "python",
-		request = "attach",
-		host = "127.0.0.1",
-		port = 5678,
-	},
+	name = "Gunicorn Test Debugger",
+	type = "python",
+	request = "attach",
+	host = "127.0.0.1",
+	port = 5678,
+	justMyCode = false,
+    gevent = true,
 }
 
 function M.setup(_)
 	require("dap-python").setup("python", {})
-	--table.insert(require("dap").configurations.python, custom_configurations)
+	table.insert(require("dap").configurations.python, custom_configurations)
 	require("dap-python").test_runner = "pytest"
 end
 
