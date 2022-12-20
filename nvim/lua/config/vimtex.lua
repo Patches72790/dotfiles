008c084d8e7e6e2163ada29f772d9d89cad12783
Patrick Harvey <patrick.harvey@wisc.edu>
2022-12-20 09:59:09 -0600

    added vimtex config

--- nvim/lua/config/vimtex.lua
+++ nvim/lua/config/vimtex.lua
@@ -1,5 +1,14 @@
 local M = {}
 
+-- Dependencies for vimtex to work properly
+--
+-- Optionally install dependency manager:
+--      tlmgr (tex live manager)
+-- Install:
+--      latexmk | latexrun (latex compiler)
+--      biber (bibliography program)
+--      zathura (linux) | skim (macos) (pdf viewer)
+
 M.setup = function()
 	if vim.fn.has("macunix") then
 		vim.g.vimtex_view_method = "skim"
