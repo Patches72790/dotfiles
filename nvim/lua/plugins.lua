5ccb0ae51f09bee3a050d7f715a4d49893748bd3
Patrick Harvey <patrick.harvey@wisc.edu>
2023-10-30 23:59:48 -0500

    removed old scripts

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -123,4 +123,24 @@ return {
 			require("neoscroll").setup()
 		end,
 	},
+
+	-- lazy.nvim
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("config.noice").setup()
+		end,
+		opts = {
+			-- add any options here
+		},
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+	},
 }
