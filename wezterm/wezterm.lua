31a8918836edad11b375c85fc3bd032f29bd5df2
Patrick Harvey <patrick.harvey@wisc.edu>
2024-03-14 15:40:28 -0500

    added wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.color_scheme = "Gruvbox Dark (Gogh)"
+--config.font = wezterm.font("Fira Code")
+
+return config
