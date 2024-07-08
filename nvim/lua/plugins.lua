9146930c1f9549df89dd83f473b1a27a21727c46
Patrick Harvey <patrick.harvey@wisc.edu>
2024-07-08 15:52:29 -0500

    update aliases

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -172,4 +172,15 @@ return {
 			"rcarriga/nvim-notify",
 		},
 	},
+
+	{
+		"toppair/peek.nvim",
+		event = { "VeryLazy" },
+		build = "deno task --quiet build:fast",
+		config = function()
+			require("peek").setup()
+			vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+			vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+		end,
+	},
 }
