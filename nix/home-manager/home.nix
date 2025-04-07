3ee9cceaca1aa20187dc2b69c2d946ad4d2c6703
Patrick Harvey <patches72790@gmail.com>
2025-04-07 13:55:46 -0500

    nix: update home file

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -47,21 +47,11 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
     # TODO - Figure out how to load dotfiles directly
-    #".config/tmux/tmux.conf".source = "/home/patroclus/dotfiles/tmux/tmux.conf";
-    #".config/alacritty/alacritty.toml".source = "/home/patroclus/dotfiles/alacritty/alacritty.toml";
-    #".zshrc".source = "/home/patroclus/dotfiles/zsh/zshrc";
-
-    # # You can also set the file content immediately.
-    # " .gradle/gradle.properties ".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    ".vimrc".source = ./dotfiles/vimrc;
+    ".config/tmux/tmux.conf".source = ./dotfiles/tmux.conf;
+    ".config/alacritty/alacritty.toml".source = ./dotfiles/alacritty/alacritty.toml;
+    ".zshrc".source = ./dotfiles/zsh/zshrc;
   };
 
   # Home Manager can also manage your environment variables through
